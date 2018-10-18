5b050b9b364bd4141b0502c69a1eac303146a5d6
Gilles De Pessemier <gilles.depessemier2@hotmail.com>
2018-10-18 22:14:08 +0200

    Tekst kan in excel document worden gezet.

--- Test form/frmtest/frmTest.Designer.cs
+++ Test form/frmtest/frmTest.Designer.cs
@@ -28,33 +28,207 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtText = new System.Windows.Forms.TextBox();
+            this.txtGetal = new System.Windows.Forms.TextBox();
+            this.txtVeelText = new System.Windows.Forms.RichTextBox();
+            this.txtMaskedText = new System.Windows.Forms.MaskedTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dgvTest = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnOpslaan1 = new System.Windows.Forms.Button();
+            this.btnOpslaan2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTest)).BeginInit();
             this.SuspendLayout();
             // 
-            // textBox1
+            // label1
             // 
-            this.textBox1.Location = new System.Drawing.Point(679, 296);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 0;
-            this.textBox1.Text = "Homo";
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Text";
             // 
-            // Form1
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Getal";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Veel text";
+            // 
+            // txtText
+            // 
+            this.txtText.Location = new System.Drawing.Point(72, 41);
+            this.txtText.Name = "txtText";
+            this.txtText.Size = new System.Drawing.Size(100, 20);
+            this.txtText.TabIndex = 3;
+            this.txtText.Text = "Test";
+            // 
+            // txtGetal
+            // 
+            this.txtGetal.Location = new System.Drawing.Point(72, 68);
+            this.txtGetal.Name = "txtGetal";
+            this.txtGetal.Size = new System.Drawing.Size(100, 20);
+            this.txtGetal.TabIndex = 4;
+            // 
+            // txtVeelText
+            // 
+            this.txtVeelText.Location = new System.Drawing.Point(72, 95);
+            this.txtVeelText.Name = "txtVeelText";
+            this.txtVeelText.Size = new System.Drawing.Size(100, 54);
+            this.txtVeelText.TabIndex = 5;
+            this.txtVeelText.Text = "";
+            // 
+            // txtMaskedText
+            // 
+            this.txtMaskedText.Location = new System.Drawing.Point(72, 155);
+            this.txtMaskedText.Name = "txtMaskedText";
+            this.txtMaskedText.Size = new System.Drawing.Size(100, 20);
+            this.txtMaskedText.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 155);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Masked text";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnOpslaan1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtMaskedText);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtVeelText);
+            this.groupBox1.Controls.Add(this.txtText);
+            this.groupBox1.Controls.Add(this.txtGetal);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 238);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnOpslaan2);
+            this.groupBox2.Controls.Add(this.dgvTest);
+            this.groupBox2.Location = new System.Drawing.Point(250, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(438, 273);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // dgvTest
+            // 
+            this.dgvTest.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTest.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dgvTest.Location = new System.Drawing.Point(29, 41);
+            this.dgvTest.Name = "dgvTest";
+            this.dgvTest.Size = new System.Drawing.Size(376, 150);
+            this.dgvTest.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Column3";
+            this.Column3.Name = "Column3";
+            // 
+            // btnOpslaan1
+            // 
+            this.btnOpslaan1.Location = new System.Drawing.Point(12, 198);
+            this.btnOpslaan1.Name = "btnOpslaan1";
+            this.btnOpslaan1.Size = new System.Drawing.Size(140, 23);
+            this.btnOpslaan1.TabIndex = 8;
+            this.btnOpslaan1.Text = "Opslaan in Excel";
+            this.btnOpslaan1.UseVisualStyleBackColor = true;
+            this.btnOpslaan1.Click += new System.EventHandler(this.btnOpslaan1_Click);
+            // 
+            // btnOpslaan2
+            // 
+            this.btnOpslaan2.Location = new System.Drawing.Point(29, 214);
+            this.btnOpslaan2.Name = "btnOpslaan2";
+            this.btnOpslaan2.Size = new System.Drawing.Size(138, 23);
+            this.btnOpslaan2.TabIndex = 1;
+            this.btnOpslaan2.Text = "Opslaan in Excel";
+            this.btnOpslaan2.UseVisualStyleBackColor = true;
+            // 
+            // frmTest
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmTest";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Test form.";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTest)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtText;
+        private System.Windows.Forms.TextBox txtGetal;
+        private System.Windows.Forms.RichTextBox txtVeelText;
+        private System.Windows.Forms.MaskedTextBox txtMaskedText;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnOpslaan1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnOpslaan2;
+        private System.Windows.Forms.DataGridView dgvTest;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
     }
 }
 
--- Test form/frmtest/frmTest.cs
+++ Test form/frmtest/frmTest.cs
@@ -7,14 +7,94 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.IO;
 
 namespace frmtest
 {
     public partial class frmTest : Form
     {
+        Microsoft.Office.Interop.Excel.Application xlexcel;
+        Microsoft.Office.Interop.Excel.Workbook xlWorkBook;
+        Microsoft.Office.Interop.Excel.Worksheet xlWorkSheet;
+        object misValue = System.Reflection.Missing.Value;
+        string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
         public frmTest()
         {
             InitializeComponent();
+        }
+
+        private void btnOpslaan1_Click(object sender, EventArgs e)
+        {
+            //exel openen
+            xlexcel = new Excel.Application();
+            xlexcel.Visible = true;
+
+            //vorige resulaten verwijderen
+            if (File.Exists(path + @"\Resultaat.xlsx"))
+                File.Delete(path + @"\Resultaat.xlsx");
+            if (File.Exists(path + @"\TempFile.xlsx"))
+            {
+                File.Delete(path + @"\TempFile.xlsx");
+
+                //leeg excel document aanmaken.
+                var app = new Microsoft.Office.Interop.Excel.Application();
+                var wb = app.Workbooks.Add();
+                wb.SaveAs(path + @"\TempFile.xlsx");
+                wb.Close();
+            }
+            else
+            {
+                //leeg excel document aanmaken.
+                var app = new Microsoft.Office.Interop.Excel.Application();
+                var wb = app.Workbooks.Add();
+                wb.SaveAs(path + @"\TempFile.xlsx");
+                wb.Close();
+            }
+
+
+            //Bestand openen en wijzigen.
+            xlWorkBook = xlexcel.Workbooks.Open(path + @"\TempFile.xlsx", 0, true, 5, "", "", true,
+            Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+            //wijzigingen
+            xlWorkSheet.Cells[1, 1] = "Kolom 1";
+            xlWorkSheet.Cells[2, 1] = txtText.Text;
+            xlWorkSheet.Cells[1, 2] = "Kolom 2";
+            xlWorkSheet.Cells[1, 3] = "Kolom 3";
+            xlWorkSheet.Cells[1, 4] = "Kolom 4";
+
+            //opslaan als..
+            xlWorkBook.Close(true, path + @"\Resultaat.xlsx", misValue);
+            xlexcel.Quit();
+
+            //document terug sluiten
+            releaseObject(xlWorkSheet);
+            releaseObject(xlWorkBook);
+            releaseObject(xlexcel);
+
+            //verwijder temp file
+            if (File.Exists(path + @"\TempFile.xlsx"))
+                File.Delete(path + @"\TempFile.xlsx");
+        }
+        private void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+                MessageBox.Show("Unable to release the Object " + ex.ToString());
+            }
+            finally
+            {
+                GC.Collect();
+            }
         }
     }
 }
