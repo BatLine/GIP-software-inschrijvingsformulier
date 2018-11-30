b910431754fae85a46020dc12ddff334607d39cb
Rune De Bruyne <rune.de.bruyne@hotmail.com>
2018-11-30 14:47:11 +0100

    Classes aangemaakt

---
+++ Test form/DefinitiefProgram/Leerling.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefinitiefProgram
+{
+    class Leerling
+    {
+        private string strNaam;
+        private string strVoornaam;
+        private string strBijkNaam;
+        private string strGeslacht;
+        private string strGeboorteplaats;
+        private string strGeboortedatum;
+        private int intRijkregisternummer;
+        private string strNationaliteit;
+        private string strGSM_Nummer;
+        private string strE_Mail;
+        private string strStraat;
+        private string strHuisnummer;
+        private string strBus;
+        private string strGemeente;
+        private string strPostcode;
+        private string strLand;
+        private int intStudieKeuzeID;
+        private int intMiddelbaar;
+        private int intSchoolstatuutID;
+
+        public string StrNaam { get => strNaam; set => strNaam = value; }
+        public string StrVoornaam { get => strVoornaam; set => strVoornaam = value; }
+        public string StrBijkNaam { get => strBijkNaam; set => strBijkNaam = value; }
+        public string StrGeslacht { get => strGeslacht; set => strGeslacht = value; }
+        public string StrGeboorteplaats { get => strGeboorteplaats; set => strGeboorteplaats = value; }
+        public string StrGeboortedatum { get => strGeboortedatum; set => strGeboortedatum = value; }
+        public int IntRijkregisternummer { get => intRijkregisternummer; set => intRijkregisternummer = value; }
+        public string StrNationaliteit { get => strNationaliteit; set => strNationaliteit = value; }
+        public string StrGSM_Nummer { get => strGSM_Nummer; set => strGSM_Nummer = value; }
+        public string StrE_Mail { get => strE_Mail; set => strE_Mail = value; }
+        public string StrStraat { get => strStraat; set => strStraat = value; }
+        public string StrHuisnummer { get => strHuisnummer; set => strHuisnummer = value; }
+        public string StrBus { get => strBus; set => strBus = value; }
+        public string StrGemeente { get => strGemeente; set => strGemeente = value; }
+        public string StrPostcode { get => strPostcode; set => strPostcode = value; }
+        public string StrLand { get => strLand; set => strLand = value; }
+        public int IntStudieKeuzeID { get => intStudieKeuzeID; set => intStudieKeuzeID = value; }
+        public int IntMiddelbaar { get => intMiddelbaar; set => intMiddelbaar = value; }
+        public int IntSchoolstatuutID { get => intSchoolstatuutID; set => intSchoolstatuutID = value; }
+    }
+}
---
+++ Test form/DefinitiefProgram/Percistence.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace DefinitiefProgram
+{
+    class Percistence
+    {
+        string strConnectionString;
+
+        public Percistence()
+        {
+            strConnectionString = "server = ID191774_6itngip9.db.webhosting.be; user id = ID191774_6itngip9; database = ID191774_6itngip9;password=";
+        }
+
+        public List<>
+    }
+}
