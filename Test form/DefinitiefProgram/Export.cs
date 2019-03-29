b8abac567a9af83789a97bdcb6a169de20517a75
Gilles De Pessemier <gilles.depessemier2@hotmail.com>
2019-03-29 14:38:19 +0100

    export defitger

--- Test form/DefinitiefProgram/Export.cs
+++ Test form/DefinitiefProgram/Export.cs
@@ -7,12 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using prop = DefinitiefProgram.Properties.Settings;
+using System.IO;
 
 namespace DefinitiefProgram
 {
     public partial class Export : Form
     {
         Business b = new Business();
+        Excel.Application xlexcel;
+        Excel.Workbook xlWorkBook;
+        Excel.Worksheet xlWorkSheet;
+        object misValue = System.Reflection.Missing.Value;
+        string tempPath = Path.GetTempPath();
+        List<Leerling> lijstSpecifieker;
+        List<Leerling> leerlingenOmteExporteren = new List<Leerling>();
         //item.Selected = true; toevoegen bij alles dat wordt toegevoegd in de listview
         //size klein 318; 140
         //size med 318; 270
@@ -58,11 +68,12 @@ namespace DefinitiefProgram
             //new thread & loading dinges?
             Tuple<int, List<Leerling>> tuple = b.getAantalLLN(van, tot);
             lblAantalLLN.Text = tuple.Item1 + " Leerlingen in deze periode.";
+            lijstSpecifieker = tuple.Item2;
             vulSpeciefieker(tuple.Item2);
         }
         void vulSpeciefieker(List<Leerling> lln)
         {
-            lvSpecifieker.Clear();
+            lvSpecifieker.Items.Clear();
             foreach (Leerling l in lln)
             {
                 ListViewItem lvi = new ListViewItem(l.StrNaam + " " + l.StrVoornaam);
@@ -136,6 +147,138 @@ namespace DefinitiefProgram
         {
             //laad scherm tonen tot einde adhv async method
             //alles uit menu naar hier halen om te exporten
+
+            if (chkSpecifiker.Checked)
+            {
+                ListView.CheckedListViewItemCollection geselecteerdeLLN = this.lvSpecifieker.CheckedItems;
+                foreach (ListViewItem item in geselecteerdeLLN)
+                {
+                    foreach (Leerling l in lijstSpecifieker)
+                    {
+                        if ((l.StrNaam + " " + l.StrVoornaam == item.Text) && (l.StrPostcode == item.SubItems[1].Text))
+                        { leerlingenOmteExporteren.Add(l); }
+                    }
+                }
+            } else { leerlingenOmteExporteren = lijstSpecifieker; }
+
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.SelectedPath = prop.Default.lastSaveFolder;
+            DialogResult result = fbd.ShowDialog();
+
+            if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+            {
+                prop.Default.lastSaveFolder = fbd.SelectedPath + @"\"; prop.Default.Save();
+                if (export(prop.Default.lastSaveFolder, "Leerlingen.xlsx"))
+                { MessageBox.Show("Leerlingen ge-exporteerd.", "", MessageBoxButtons.OK, MessageBoxIcon.Information); }
+                else { MessageBox.Show("Leerlingen exporteren mislukt.", "", MessageBoxButtons.OK, MessageBoxIcon.Warning); }
+            }
+        }
+        bool export(string locatie, string naam)
+        {
+            int intTeller = 1;
+            //alles in excel zetten
+            //exel openen
+            xlexcel = new Excel.Application();
+            xlexcel.Visible = false;
+
+            //vorige resulaten verwijderen
+            if (File.Exists(locatie + naam))
+                File.Delete(locatie + naam);
+            if (File.Exists(tempPath + @"\tempLeerlingen.xlsx"))
+            { File.Delete(tempPath + @"\tempLeerlingen.xlsx"); }
+            //leeg excel document aanmaken.
+            var app = new Excel.Application();
+            var wb = app.Workbooks.Add();
+            wb.SaveAs(tempPath + @"tempLeerlingen.xlsx");
+            wb.Close();
+
+            //Bestand openen en wijzigen.
+            xlWorkBook = xlexcel.Workbooks.Open(tempPath + @"\tempLeerlingen.xlsx", 0, true, 5, "", "", true,
+            Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+            foreach (Leerling l in leerlingenOmteExporteren)
+            {
+                xlWorkSheet.Cells[intTeller, 1] = l.StrNaam;
+                xlWorkSheet.Cells[intTeller, 2] = l.StrVoornaam;
+                xlWorkSheet.Cells[intTeller, 3] = l.StrBijkNaam;
+                xlWorkSheet.Cells[intTeller, 4] = l.StrGeslacht;
+                xlWorkSheet.Cells[intTeller, 5] = l.StrGeboortedatum;
+                xlWorkSheet.Cells[intTeller, 6] = l.StrGeboorteplaats;
+                xlWorkSheet.Cells[intTeller, 7] = l.StrRijkregisternummer;
+                xlWorkSheet.Cells[intTeller, 8] = l.StrStraat;
+                xlWorkSheet.Cells[intTeller, 9] = l.StrHuisnummer;
+                xlWorkSheet.Cells[intTeller, 10] = l.StrBus;
+                xlWorkSheet.Cells[intTeller, 11] = l.StrPostcode;
+                xlWorkSheet.Cells[intTeller, 12] = l.StrGemeente;
+                xlWorkSheet.Cells[intTeller, 13] = l.StrLand;
+                xlWorkSheet.Cells[intTeller, 14] = l.StrNationaliteit;
+                if (l.O.StrGezinshoofd == "Moeder")
+                { xlWorkSheet.Cells[intTeller, 15] = l.O.StrTelefoonWerkMoeder; xlWorkSheet.Cells[intTeller, 16] = l.O.StrGSMMoeder; }
+                else { xlWorkSheet.Cells[intTeller, 15] = l.O.StrTelefoonWerkVader; xlWorkSheet.Cells[intTeller, 16] = l.O.StrGSMVader; }
+                xlWorkSheet.Cells[intTeller, 17] = l.StrGSM_Nummer;
+                xlWorkSheet.Cells[intTeller, 18] = l.O.StrGSMMoeder; //gsm ouders
+                xlWorkSheet.Cells[intTeller, 19] = l.O.StrGSMVader; //gsm ouders
+                xlWorkSheet.Cells[intTeller, 20] = l.StrE_Mail;
+                xlWorkSheet.Cells[intTeller, 21] = l.O.StrEmailMoeder; //email ouders
+                xlWorkSheet.Cells[intTeller, 22] = l.O.StrEmailVader; //email ouders
+
+                //aanmeldingstijdstip
+                //klascode
+                //klasnr
+
+                xlWorkSheet.Cells[intTeller, 23] = l.StrGebruikersnaamNetwerk;
+                xlWorkSheet.Cells[intTeller, 24] = l.StrWachtwoordNetwerk;
+
+                xlWorkSheet.Cells[intTeller, 25] = l.O.StrNaamMoeder; //naam
+                xlWorkSheet.Cells[intTeller, 26] = l.O.StrNaamMoeder; //voornaam
+                xlWorkSheet.Cells[intTeller, 27] = l.O.StrGeboorteDatumMoeder; //nog invullen
+                xlWorkSheet.Cells[intTeller, 28] = l.O.StrRijksregisterNRMoeder; //nog invullen
+                xlWorkSheet.Cells[intTeller, 29] = l.O.StrBeroepMoeder; //nog invullen
+                xlWorkSheet.Cells[intTeller, 30] = l.O.StrGSMMoeder;
+                xlWorkSheet.Cells[intTeller, 31] = l.O.StrTelefoonWerkMoeder;
+                xlWorkSheet.Cells[intTeller, 32] = l.O.StrEmailMoeder;
+
+                xlWorkSheet.Cells[intTeller, 33] = l.O.StrNaamVader; //naam
+                xlWorkSheet.Cells[intTeller, 34] = l.O.StrNaamVader; //voornaam
+                xlWorkSheet.Cells[intTeller, 35] = l.O.StrGeboorteDatumVader; //nog invullen
+                xlWorkSheet.Cells[intTeller, 36] = l.O.StrRijksregisterNRVader; //nog invullen
+                xlWorkSheet.Cells[intTeller, 37] = l.O.StrBeroepVader; //nog invullen
+                xlWorkSheet.Cells[intTeller, 38] = l.O.StrGSMVader;
+                xlWorkSheet.Cells[intTeller, 39] = l.O.StrTelefoonWerkVader;
+                xlWorkSheet.Cells[intTeller, 40] = l.O.StrEmailVader;
+
+                //stiefmoeder
+                //stiefvader
+
+                intTeller++;
+            }
+
+            //opslaan als..
+            xlWorkBook.Close(true, locatie + naam, misValue);
+            xlexcel.Quit();
+
+            //document terug sluiten
+            releaseObject(xlWorkSheet);
+            releaseObject(xlWorkBook);
+            releaseObject(xlexcel);
+
+            return true;
+        }
+        private void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+                MessageBox.Show("Unable to release the Object " + ex.ToString());
+            }
+            finally
+            { GC.Collect(); }
         }
     }
 }
